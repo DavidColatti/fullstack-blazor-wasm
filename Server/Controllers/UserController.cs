dd177ad302e7f4eeb0c940e535a259cb94dae358
David Colatti <davidcolatti@gmail.com>
2021-01-18 10:00:02 -0500

    feat: initial commit and shared models

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using fullstackblazorwasm.Server.Controllers;
+using fullstackblazorwasm.Server.Models;
+using System.Collections.Generic;
+using fullstackblazorwasm.Shared;
+using System.Linq;
+
+namespace fullstackblazorwasm.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserContext _userContext;
+
+        public UserController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        // GET api/user
+        [HttpGet("")]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return _userContext.Users.ToList();
+        }
+
+        // GET api/user/5
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            return _userContext.Users.FirstOrDefault(user => user.Id == id);
+        }
+
+        // POST api/user
+        [HttpPost("")]
+        public ActionResult<User> PostUser(User user)
+        {
+            _userContext.Users.Add(user);
+            _userContext.SaveChanges();
+            return user;
+        }
+    }
+}
---
+++ Server/Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using fullstackblazorwasm.Shared;
+
+namespace fullstackblazorwasm.Server.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
